fc3c05123ba7b836edc2fbbd083070c3ce4358a0
YoonsikYu <168201696+YoonsikYu@users.noreply.github.com>
2024-08-19 21:42:07 +0200

    update 3333

--- src/task3.py
+++ src/task3.py
@@ -50,4 +50,4 @@ output_path = os.path.join('output', 'department_breakdown')
 df_with_formatted_sales.coalesce(1).write.mode("overwrite").option("header", True).csv(output_path)
 
 spark.stop()  
- 
+  
