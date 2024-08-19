2a9c51020c5b7bd6b60735339cca43af7a208deb
YoonsikYu <168201696+YoonsikYu@users.noreply.github.com>
2024-08-19 13:38:38 +0200

    run

--- src/task1.py
+++ src/task1.py
@@ -1,12 +1,13 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+import os
 
 spark = SparkSession.builder \
     .appName("TASK1") \
     .getOrCreate()
 
-csv_file_path1 = r"C:\Users\SAMSUNG\Documents\GitHub\assignment\data\dataset_one.csv"
-csv_file_path2 = r"C:\Users\SAMSUNG\Documents\GitHub\assignment\data\dataset_two.csv"
+csv_file_path1 = os.path.join('data', 'dataset_one.csv')
+csv_file_path2 = os.path.join('data', 'dataset_two.csv')
 
 df1 = spark.read.csv(csv_file_path1, header=True, inferSchema=True)
 df2 = spark.read.csv(csv_file_path2, header=True, inferSchema=True)
@@ -14,7 +15,7 @@ df2 = spark.read.csv(csv_file_path2, header=True, inferSchema=True)
 df = df1.join(df2, on='id', how = 'inner')
 IT_df = df.filter(col('area')=='IT').orderBy(col('sales_amount').desc()).limit(100)
 
-output_path = r"C:\Users\SAMSUNG\Documents\GitHub\assignment\output\it_data"
+output_path = os.path.join('output', 'it_data')
 
 IT_df.coalesce(1).write.mode("overwrite").option("header", True).csv(output_path)
 
