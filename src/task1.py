2581e297bf350e337e95f393d34ec7ec099fb400
YoonsikYu <168201696+YoonsikYu@users.noreply.github.com>
2024-08-19 13:07:08 +0200

    structure changes

--- src/task1.py
+++ src/task1.py
@@ -5,16 +5,16 @@ spark = SparkSession.builder \
     .appName("TASK1") \
     .getOrCreate()
 
-csv_file_path1 = r"C:\Users\SAMSUNG\Documents\assignment\dataset_one.csv"
-csv_file_path2 = r"C:\Users\SAMSUNG\Documents\assignment\dataset_two.csv"
- 
+csv_file_path1 = r"C:\Users\SAMSUNG\Documents\GitHub\assignment\data\dataset_one.csv"
+csv_file_path2 = r"C:\Users\SAMSUNG\Documents\GitHub\assignment\data\dataset_two.csv"
+
 df1 = spark.read.csv(csv_file_path1, header=True, inferSchema=True)
 df2 = spark.read.csv(csv_file_path2, header=True, inferSchema=True)
 
 df = df1.join(df2, on='id', how = 'inner')
 IT_df = df.filter(col('area')=='IT').orderBy(col('sales_amount').desc()).limit(100)
 
-output_path = r"C:\Users\SAMSUNG\Documents\it_data"
+output_path = r"C:\Users\SAMSUNG\Documents\GitHub\assignment\output\it_data"
 
 IT_df.coalesce(1).write.mode("overwrite").option("header", True).csv(output_path)
 
