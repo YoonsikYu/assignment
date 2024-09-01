161fc6ce7f1a660d0aaaae5182d5dab2359cf0d2
YoonsikYu <168201696+YoonsikYu@users.noreply.github.com>
2024-09-02 01:12:15 +0200

    update

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import os
 from logging.handlers import RotatingFileHandler
 from pyspark.sql import SparkSession
 from src.task1 import process_it_data
-from src.task2 import process_marketing_address_info
-from src.task3 import process_department_breakdown
-from src.task4 import process_top_3_performers
-from src.task5 import process_top_3_most_sold
-from src.task6 import process_best_salesperson
-from src.extra_insight_one import extra_insight_one
-from src.extra_insight_two import extra_insight_two
+#from src.task2 import process_marketing_address_info
+#from src.task3 import process_department_breakdown
+#from src.task4 import process_top_3_performers
+#from src.task5 import process_top_3_most_sold
+#from src.task6 import process_best_salesperson
+#from src.extra_insight_one import extra_insight_one
+#from src.extra_insight_two import extra_insight_two
 
 def setup_logging(log_file='logs/data_processing.log') -> logging.Logger:
     """
