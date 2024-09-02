55c5da9cb2a2b6e1c7acd00f5479bbd66da1d472
YoonsikYu <168201696+YoonsikYu@users.noreply.github.com>
2024-09-02 08:44:50 +0200

    update

--- src/extra_insight_one.py
+++ src/extra_insight_one.py
@@ -1,40 +1,38 @@
 import logging
-from pyspark.sql import DataFrame
-from pyspark.sql.functions import sum, col, round, first
+from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql.functions import col, sum, round, first
 import os
 
 logger = logging.getLogger(__name__)
 
 def extra_insight_one(df1: DataFrame, df2: DataFrame, df3: DataFrame) -> DataFrame:
     """
-    Generate additional insight two by joining three datasets, categorizing data by age group,
-    calculating sales by age group, and saving the results to a CSV file.
+    Generate additional insight one by joining three datasets, performing the necessary analysis,
+    and saving the results to a CSV file.
 
-    df1: The first input DataFrame.
-    df2: The second input DataFrame.
-    df3: The third input DataFrame.
-    return: A DataFrame containing the pivoted results.
+    :param df1: The first input DataFrame.
+    :param df2: The second input DataFrame.
+    :param df3: The third input DataFrame.
+    :return: A DataFrame containing the results of the additional insight one analysis.
     """
     logger.info("Joining datasets on 'id' and 'caller_id' columns...")
     df = df1.join(df2, on='id', how='inner')
     df_join = df3.join(df, df3.caller_id == df.id, 'left')
 
-    logger.info("Aggregating total quantity and sales amount...")
+    logger.info("Aggregating data for products sold and calculating product price...")
     dftest1 = df_join.groupBy('product_sold', 'country').agg(
         sum('quantity').alias('total_quantity'),
         sum('sales_amount').alias('total_sales_amount')
     )
-    
-    logger.info("Calculating product price...")
     dftest1 = dftest1.withColumn('product_price', round(col('total_sales_amount') / col('total_quantity'), 2))
 
-    logger.info("Aggregating total quantity sold and product price by product and country...")
+    logger.info("Aggregating total quantity sold per product and country...")
     dftest1_with_quantity = dftest1.groupBy("product_sold", "country").agg(
         sum("total_quantity").alias("total_quantity_sold"),
         first("product_price").alias("product_price")
     )
 
-    logger.info("Pivoting the data to show product prices and quantities sold by country...")
+    logger.info("Pivoting data by product and country...")
     df_pivot = dftest1_with_quantity.groupBy("product_sold").pivot("country").agg(
         first("product_price").alias("product_price"),
         first("total_quantity_sold").alias("total_quantity_sold")
