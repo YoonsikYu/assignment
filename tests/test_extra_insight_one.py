2db2cdbdee227e92feeaea666535fe04f87df2f7
YoonsikYu <168201696+YoonsikYu@users.noreply.github.com>
2024-09-02 08:59:21 +0200

    sdf

--- tests/test_extra_insight_one.py
+++ tests/test_extra_insight_one.py
@@ -24,12 +24,24 @@ def test_extra_insight_one(spark):
         (1, "Evie Godfrey van Alemannië-Smits", "1808 KR, Benningbroek", 69087.89),
         (2, "Rosa Kuipers", "Jetlaan 816, 8779 EM, Holwierde", 37606.23),
         (3, "Vincent Mathurin", "4133HB", 44933.12),
+        (4, "Sam", "1234df", 22342.13),
+        (5, "Sara", "3423ds", 23413.32),
+        (6, "Tom", "2341sd", 12342.31),
+        (7, "JP", "2123ds", 34212.36),
+        (8, "WP", "1255fd", 234029,21),
+        (9, "EE", "4442df", 237744,21)
     ], ["id", "name", "address", "sales_amount"])
 
     df3 = spark.createDataFrame([
         (1, 1, "Verbruggen-Vermeulen CommV", "Anny Claessens", 45, "Belgium", "Banner", 50),
         (2, 2, "Hendrickx CV", "Lutgarde Van Loock", 41, "Germany", "Sign", 23),
         (3, 3, "Koninklijke Aelftrud van Wessex", "Mustafa Ehlert", 34, "Netherlands", "Headset", 1),
+        (4, 4, "dd", "alek", 23, "Germany", "Banner", 32),
+        (5, 5, "ee", "sdwe", 44, "Netherlands", "Banner", 34),
+        (6, 6, "er", "weer", 36, "Belgium", "Sign", 35),
+        (7, 7, "der", "wezd", 34, "Netherlands", "Sign", 34),
+        (8, 8, "sdq", "werdza", 52, "Belgium", "Headset", 35),
+        (9, 9, "sdfe", "werers", 22, "Germany", "Headset", 11)
     ], ["id", "caller_id", "company", "recipient", "age", "country", "product_sold", "quantity"])
 
     expected_df = spark.createDataFrame([
